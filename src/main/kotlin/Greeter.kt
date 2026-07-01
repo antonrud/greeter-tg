196c81ddab1271a55eb47d556b89445432a7853f
Anton Rudacov <anton.rudacov@gmail.com>
2026-07-01 15:28:30 +0200

    Improvements

--- src/main/kotlin/Greeter.kt
+++ src/main/kotlin/Greeter.kt
@@ -1,23 +1,26 @@
 import dev.inmo.kslog.common.KSLog
 import dev.inmo.kslog.common.LogLevel
 import dev.inmo.kslog.common.defaultMessageFormatter
+import dev.inmo.kslog.common.e
+import dev.inmo.kslog.common.i
 import dev.inmo.kslog.common.setDefaultKSLog
 import dev.inmo.tgbotapi.extensions.api.send.send
 import dev.inmo.tgbotapi.extensions.api.telegramBot
 import dev.inmo.tgbotapi.extensions.behaviour_builder.buildBehaviourWithLongPolling
 import dev.inmo.tgbotapi.extensions.behaviour_builder.triggers_handling.onChatMemberJoined
+import dev.inmo.tgbotapi.types.LinkPreviewOptions
 import dev.inmo.tgbotapi.utils.PreviewFeature
 
+
+private val log = KSLog("greeter-tg")
 
 fun loadGreeting(): String =
     object {}.javaClass.getResourceAsStream("/greeting.txt")!!.bufferedReader().readText().trim()
 
 @OptIn(PreviewFeature::class)
 suspend fun main(args: Array<String>) {
-
     val greeting = loadGreeting()
     val token = args.first()
-
     val isDebug = args.any { it == "debug" }
 
     if (isDebug) {
@@ -30,13 +33,25 @@ suspend fun main(args: Array<String>) {
 
     val bot = telegramBot(token)
 
-    bot.buildBehaviourWithLongPolling {
+    log.i { "Application started" }
 
+    bot.buildBehaviourWithLongPolling {
         onChatMemberJoined {
-            send(
-                chatId = it.chat.id,
-                text = greeting.replace("{username}", it.member.username.toString())
-            )
+            runCatching {
+
+                val name = it.member.username?.username ?: it.member.firstName
+                log.i { "New member joined chat ${it.chat.id}: $name" }
+
+                send(
+                    chatId = it.chat.id,
+                    text = greeting.replace("{username}", name),
+                    linkPreviewOptions = LinkPreviewOptions.Disabled
+                )
+            }.onFailure { ex ->
+                log.e(ex) {
+                    "Failed to greet new member in chat ${it.chat.id}"
+                }
+            }
         }
     }.join()
 }
